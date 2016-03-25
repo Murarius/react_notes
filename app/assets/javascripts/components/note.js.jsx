946753430019354bf841ac650efdf92c8d2440bc
Murarius <murarius2011@gmail.com>
2016-03-25 19:46:45 +0100

    add buttons to note componnents + note style

--- app/assets/javascripts/components/note.js.jsx
+++ app/assets/javascripts/components/note.js.jsx
@@ -9,10 +9,22 @@ var Note = React.createClass({
       null
     };
   },
+  edit: function() {
+    alert('Editing Note');
+  },
+  remove: function() {
+    alert('Removing Note');
+  },
   render: function() {
     return (
-  //    <div className='note'>{ this.props.record.body}</div>
-   <div className='note'>{ this.state.record.body }</div>
+     // <div className='note'>{ this.props.record.body}</div>
+     <div className='note'>
+       <span className='buttons'>
+         <a onClick={ this.edit } className='edit'><i className='fa fa-pencil'></i></a>
+         <a onClick={ this.remove } className='delete'><i className="fa fa-times"></i></a>
+       </span>
+       { this.state.record.body }
+     </div>
     );
   }
 });
