4c7d7c022fd1b1c26f9e4fcac29625af7edf5b32
Murarius <murarius2011@gmail.com>
2016-03-27 14:32:44 +0200

    add note create action

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -2,4 +2,20 @@ class NotesController < ApplicationController
   def index
     @notes = Note.all
   end
+
+  def create
+    @note = Note.new(note_params)
+
+    if @note.save
+      render jscon: @note
+    else
+      render json: @record.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def note_params
+    params.require(:note).permit(:body)
+  end
 end
